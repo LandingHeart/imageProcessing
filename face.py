d3f1a9c86f95a98e92e0686476a94b59dc2f23b3
LandingHeart <land.liu0123@gmail.com>
2019-02-25 17:06:05 -0500

    updating

--- face.py
+++ face.py
@@ -1,36 +1,34 @@
-import cv2
+import cv2 as cv
 import numpy as np 
 import sys
 
 cascPath = sys.argv[0]
-faceCascade = cv2.CascadeClassifier(cascPath)
-
-video_capture = cv2.VideoCapture(0)
+face_cascade = cv.CascadeClassifier('/Users/shinan/Desktop/imageProcessing/data/haarcascades/haarcascade_frontalface_default.xml')
+video_capture = cv.VideoCapture(0)
 
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale(
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
+        flags=cv.CASCADE_SCALE_IMAGE
     )
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     # Display the resulting frame
-    cv2.imshow('Video', frame)
+    cv.imshow('Video', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything is done, release the capture
 video_capture.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
